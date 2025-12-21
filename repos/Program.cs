8bc7d6aebb703d9dbd8d092ad2746cacf18fa574
Ahmadou Gassam <melodygazzam1@gmail.com>
2025-12-20 18:29:40 -0600

    first commit

---
+++ repos/Program.cs
@@ -0,0 +1,200 @@
+﻿//using system;
+//using system.collections.generic;
+//using system.linq;
+//using system.text;
+//using system.threading.tasks;
+
+//namespace pract2025
+//{
+//    internal class program
+//    {
+//        static void main(string[] args)
+//        {
+//            char[] letters = { 'a', 'h', 'm', 'a', 'd', 'o', 'u', ' ', 'g', 'a', 's', 's', 'a', 'm', 'a' };
+//            string name = "";
+//            int[] a = new int[letters.length];
+//            for (int i = 0; i < letters.length; i++)
+//            {
+//                name += letters[i];
+//                a[i] = i + 1;
+//                sendmessage(name, a[i]);
+//            }
+
+//            console.writeline(); // space between outputs
+
+//            // htc output (same pattern, after name)
+//            char[] htcletters = "hennepin technical college".tochararray();
+//            string htc = "";
+//            int[] b = new int[htcletters.length];
+
+//            for (int i = 0; i < htcletters.length; i++)
+//            {
+//                htc += htcletters[i];
+//                b[i] = i + 1;
+//                sendmessage(htc, b[i]);
+//            }
+
+//            console.writeline();
+//            console.writeline("program developed by ahmadou gassama");
+
+//            console.readkey();
+
+//        }
+
+//        static void sendmessage(string name, int msg)
+//        {
+//            console.writeline("hello, " + name + "! count to " + msg);
+
+//        }
+//    }
+//}
+//using system;
+//using system.collections.generic;
+//using system.linq;
+//using system.text;
+//using system.threading.tasks;
+
+//namespace pract2025
+//{
+//    internal class program
+//    {
+//        static void main(string[] args)
+//        {
+//            char[] letters = { 'a', 'h', 'm', 'a', 'd', 'o', 'u', ' ', 'g', 'a', 's', 's', 'a', 'm', 'a' };
+//            string name = "";
+//            int[] a = new int[letters.length];
+//            for (int i = 0; i < letters.length; i++)
+//            {
+//                name += letters[i];
+//                a[i] = i + 1;
+//                sendmessage(name, a[i]);
+//            }
+
+//            console.writeline(); // space between outputs
+
+//            // htc output (same pattern, after name)
+//            char[] htcletters = "hennepin technical college".tochararray();
+//            string htc = "";
+//            int[] b = new int[htcletters.length];
+
+//            for (int i = 0; i < htcletters.length; i++)
+//            {
+//                htc += htcletters[i];
+//                b[i] = i + 1;
+//                sendmessage(htc, b[i]);
+//            }
+
+//            console.writeline();
+//            console.writeline("program developed by ahmadou gassama");
+
+//            console.readkey();
+
+//        }
+
+//        static void sendmessage(string name, int msg)
+//        {
+//            console.writeline("hello, " + name + "! count to " + msg);
+
+//        }
+//    }
+//}
+//using system;
+//using system.collections.generic;
+//using system.linq;
+//using system.text;
+//using system.threading.tasks;
+
+//namespace pract2025
+//{
+//    internal class program
+//    {
+//        static void main(string[] args)
+//        {
+//            char[] letters = { 'a', 'h', 'm', 'a', 'd', 'o', 'u', ' ', 'g', 'a', 's', 's', 'a', 'm', 'a' };
+//            string name = "";
+//            int[] a = new int[letters.length];
+//            for (int i = 0; i < letters.length; i++)
+//            {
+//                name += letters[i];
+//                a[i] = i + 1;
+//                sendmessage(name, a[i]);
+//            }
+
+//            console.writeline(); // space between outputs
+
+//            // htc output (same pattern, after name)
+//            char[] htcletters = "hennepin technical college".tochararray();
+//            string htc = "";
+//            int[] b = new int[htcletters.length];
+
+//            for (int i = 0; i < htcletters.length; i++)
+//            {
+//                htc += htcletters[i];
+//                b[i] = i + 1;
+//                sendmessage(htc, b[i]);
+//            }
+
+//            console.writeline();
+//            console.writeline("program developed by ahmadou gassama");
+
+//            console.readkey();
+
+//        }
+
+//        static void sendmessage(string name, int msg)
+//        {
+//            console.writeline("hello, " + name + "! count to " + msg);
+
+//        }
+//    }
+//}
+//using system;
+//using system.collections.generic;
+//using system.linq;
+//using system.text;
+//using system.threading.tasks;
+
+//namespace pract2025
+//{
+//    internal class program
+//    {
+//        static void main(string[] args)
+//        {
+//            char[] letters = { 'a', 'h', 'm', 'a', 'd', 'o', 'u', ' ', 'g', 'a', 's', 's', 'a', 'm', 'a' };
+//            string name = "";
+//            int[] a = new int[letters.length];
+//            for (int i = 0; i < letters.length; i++)
+//            {
+//                name += letters[i];
+//                a[i] = i + 1;
+//                sendmessage(name, a[i]);
+//            }
+
+//            console.writeline(); // space between outputs
+
+//            // htc output (same pattern, after name)
+//            char[] htcletters = "hennepin technical college".tochararray();
+//            string htc = "";
+//            int[] b = new int[htcletters.length];
+
+//            for (int i = 0; i < htcletters.length; i++)
+//            {
+//                htc += htcletters[i];
+//                b[i] = i + 1;
+//                sendmessage(htc, b[i]);
+//            }
+
+//            console.writeline();
+//            console.writeline("program developed by ahmadou gassama");
+
+//            console.readkey();
+
+//        }
+
+//        static void sendmessage(string name, int msg)
+//        {
+//            console.writeline("hello, " + name + "! count to " + msg);
+
+//        }
+//    }
+//}
